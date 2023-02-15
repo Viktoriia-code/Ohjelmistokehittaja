1d443c7b7d515233de71e88c0bdcc2b2c6c65001
Sisilisko <beloborodova.viktoriia@gmail.com>
2023-02-15 17:21:03 +0200

    feat: 17har functiot avaa ja uusi

---
+++ GraafiTehtavat/Harjoitus17/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Harjoitus17
+{
+    partial class Muistio
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tiedostoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uusiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.avaaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tallennaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tallennaNimelläToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tulostuksenEsikatseluToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tulostaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.poistuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.muokkaaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.kopioiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.leikkaaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.liitäToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.poistaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.valitseKaikkiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.muotoileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kirjasinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tekstinKorostusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tekstinRivittysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.apuaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tietoaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TekstilaatikkoRTB = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiedostoToolStripMenuItem,
+            this.muokkaaToolStripMenuItem,
+            this.muotoileToolStripMenuItem,
+            this.apuaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tiedostoToolStripMenuItem
+            // 
+            this.tiedostoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uusiToolStripMenuItem,
+            this.avaaToolStripMenuItem,
+            this.tallennaToolStripMenuItem,
+            this.tallennaNimelläToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.tulostuksenEsikatseluToolStripMenuItem,
+            this.tulostaToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.poistuToolStripMenuItem});
+            this.tiedostoToolStripMenuItem.Name = "tiedostoToolStripMenuItem";
+            this.tiedostoToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.tiedostoToolStripMenuItem.Text = "&Tiedosto";
+            // 
+            // uusiToolStripMenuItem
+            // 
+            this.uusiToolStripMenuItem.Name = "uusiToolStripMenuItem";
+            this.uusiToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.uusiToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.uusiToolStripMenuItem.Text = "Uusi";
+            this.uusiToolStripMenuItem.Click += new System.EventHandler(this.uusiToolStripMenuItem_Click);
+            // 
+            // avaaToolStripMenuItem
+            // 
+            this.avaaToolStripMenuItem.Name = "avaaToolStripMenuItem";
+            this.avaaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.avaaToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.avaaToolStripMenuItem.Text = "Avaa";
+            this.avaaToolStripMenuItem.Click += new System.EventHandler(this.avaaToolStripMenuItem_Click);
+            // 
+            // tallennaToolStripMenuItem
+            // 
+            this.tallennaToolStripMenuItem.Name = "tallennaToolStripMenuItem";
+            this.tallennaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.tallennaToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.tallennaToolStripMenuItem.Text = "Tallenna";
+            this.tallennaToolStripMenuItem.Click += new System.EventHandler(this.tallennaToolStripMenuItem_Click);
+            // 
+            // tallennaNimelläToolStripMenuItem
+            // 
+            this.tallennaNimelläToolStripMenuItem.Name = "tallennaNimelläToolStripMenuItem";
+            this.tallennaNimelläToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.tallennaNimelläToolStripMenuItem.Text = "Tallenna nimellä...";
+            this.tallennaNimelläToolStripMenuItem.Click += new System.EventHandler(this.tallennaNimelläToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(188, 6);
+            // 
+            // tulostuksenEsikatseluToolStripMenuItem
+            // 
+            this.tulostuksenEsikatseluToolStripMenuItem.Name = "tulostuksenEsikatseluToolStripMenuItem";
+            this.tulostuksenEsikatseluToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.tulostuksenEsikatseluToolStripMenuItem.Text = "Tulostuksen esikatselu";
+            this.tulostuksenEsikatseluToolStripMenuItem.Click += new System.EventHandler(this.tulostuksenEsikatseluToolStripMenuItem_Click);
+            // 
+            // tulostaToolStripMenuItem
+            // 
+            this.tulostaToolStripMenuItem.Name = "tulostaToolStripMenuItem";
+            this.tulostaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.tulostaToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.tulostaToolStripMenuItem.Text = "Tulosta";
+            this.tulostaToolStripMenuItem.Click += new System.EventHandler(this.tulostaToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(188, 6);
+            // 
+            // poistuToolStripMenuItem
+            // 
+            this.poistuToolStripMenuItem.Name = "poistuToolStripMenuItem";
+            this.poistuToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.poistuToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.poistuToolStripMenuItem.Text = "Poistu";
+            this.poistuToolStripMenuItem.Click += new System.EventHandler(this.poistuToolStripMenuItem_Click);
+            // 
+            // muokkaaToolStripMenuItem
+            // 
+            this.muokkaaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoToolStripMenuItem,
+            this.redoToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.kopioiToolStripMenuItem,
+            this.leikkaaToolStripMenuItem,
+            this.liitäToolStripMenuItem,
+            this.poistaToolStripMenuItem,
+            this.valitseKaikkiToolStripMenuItem});
+            this.muokkaaToolStripMenuItem.Name = "muokkaaToolStripMenuItem";
+            this.muokkaaToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.muokkaaToolStripMenuItem.Text = "&Muokkaa";
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.undoToolStripMenuItem.Text = "Undo";
+            // 
+            // redoToolStripMenuItem
+            // 
+            this.redoToolStripMenuItem.Name = "redoToolStripMenuItem";
+            this.redoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Y)));
+            this.redoToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.redoToolStripMenuItem.Text = "Redo";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(179, 6);
+            // 
+            // kopioiToolStripMenuItem
+            // 
+            this.kopioiToolStripMenuItem.Name = "kopioiToolStripMenuItem";
+            this.kopioiToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.kopioiToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.kopioiToolStripMenuItem.Text = "Kopioi";
+            // 
+            // leikkaaToolStripMenuItem
+            // 
+            this.leikkaaToolStripMenuItem.Name = "leikkaaToolStripMenuItem";
+            this.leikkaaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.leikkaaToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.leikkaaToolStripMenuItem.Text = "Leikkaa";
+            // 
+            // liitäToolStripMenuItem
+            // 
+            this.liitäToolStripMenuItem.Name = "liitäToolStripMenuItem";
+            this.liitäToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.liitäToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.liitäToolStripMenuItem.Text = "Liitä";
+            // 
+            // poistaToolStripMenuItem
+            // 
+            this.poistaToolStripMenuItem.Name = "poistaToolStripMenuItem";
+            this.poistaToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.poistaToolStripMenuItem.Text = "Poista";
+            // 
+            // valitseKaikkiToolStripMenuItem
+            // 
+            this.valitseKaikkiToolStripMenuItem.Name = "valitseKaikkiToolStripMenuItem";
+            this.valitseKaikkiToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.valitseKaikkiToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.valitseKaikkiToolStripMenuItem.Text = "Valitse kaikki";
+            // 
+            // muotoileToolStripMenuItem
+            // 
+            this.muotoileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kirjasinToolStripMenuItem,
+            this.tekstinKorostusToolStripMenuItem,
+            this.tekstinRivittysToolStripMenuItem});
+            this.muotoileToolStripMenuItem.Name = "muotoileToolStripMenuItem";
+            this.muotoileToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.muotoileToolStripMenuItem.Text = "&Muotoile";
+            // 
+            // kirjasinToolStripMenuItem
+            // 
+            this.kirjasinToolStripMenuItem.Name = "kirjasinToolStripMenuItem";
+            this.kirjasinToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.kirjasinToolStripMenuItem.Text = "Kirjasin";
+            // 
+            // tekstinKorostusToolStripMenuItem
+            // 
+            this.tekstinKorostusToolStripMenuItem.Name = "tekstinKorostusToolStripMenuItem";
+            this.tekstinKorostusToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.tekstinKorostusToolStripMenuItem.Text = "Tekstin korostus";
+            // 
+            // tekstinRivittysToolStripMenuItem
+            // 
+            this.tekstinRivittysToolStripMenuItem.Name = "tekstinRivittysToolStripMenuItem";
+            this.tekstinRivittysToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.tekstinRivittysToolStripMenuItem.Text = "Tekstin rivittys";
+            // 
+            // apuaToolStripMenuItem
+            // 
+            this.apuaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tietoaToolStripMenuItem});
+            this.apuaToolStripMenuItem.Name = "apuaToolStripMenuItem";
+            this.apuaToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.apuaToolStripMenuItem.Text = "&Apua";
+            // 
+            // tietoaToolStripMenuItem
+            // 
+            this.tietoaToolStripMenuItem.Name = "tietoaToolStripMenuItem";
+            this.tietoaToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.tietoaToolStripMenuItem.Text = "Tietoa";
+            // 
+            // TekstilaatikkoRTB
+            // 
+            this.TekstilaatikkoRTB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TekstilaatikkoRTB.Location = new System.Drawing.Point(0, 24);
+            this.TekstilaatikkoRTB.Name = "TekstilaatikkoRTB";
+            this.TekstilaatikkoRTB.Size = new System.Drawing.Size(800, 426);
+            this.TekstilaatikkoRTB.TabIndex = 1;
+            this.TekstilaatikkoRTB.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Muistio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TekstilaatikkoRTB);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Muistio";
+            this.Text = "Muistio";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem tiedostoToolStripMenuItem;
+        private ToolStripMenuItem uusiToolStripMenuItem;
+        private RichTextBox TekstilaatikkoRTB;
+        private ToolStripMenuItem avaaToolStripMenuItem;
+        private ToolStripMenuItem tallennaToolStripMenuItem;
+        private ToolStripMenuItem tallennaNimelläToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem2;
+        private ToolStripMenuItem tulostuksenEsikatseluToolStripMenuItem;
+        private ToolStripMenuItem tulostaToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem poistuToolStripMenuItem;
+        private ToolStripMenuItem muokkaaToolStripMenuItem;
+        private ToolStripMenuItem undoToolStripMenuItem;
+        private ToolStripMenuItem redoToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem3;
+        private ToolStripMenuItem kopioiToolStripMenuItem;
+        private ToolStripMenuItem leikkaaToolStripMenuItem;
+        private ToolStripMenuItem liitäToolStripMenuItem;
+        private ToolStripMenuItem poistaToolStripMenuItem;
+        private ToolStripMenuItem valitseKaikkiToolStripMenuItem;
+        private ToolStripMenuItem muotoileToolStripMenuItem;
+        private ToolStripMenuItem kirjasinToolStripMenuItem;
+        private ToolStripMenuItem tekstinKorostusToolStripMenuItem;
+        private ToolStripMenuItem tekstinRivittysToolStripMenuItem;
+        private ToolStripMenuItem apuaToolStripMenuItem;
+        private ToolStripMenuItem tietoaToolStripMenuItem;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ GraafiTehtavat/Harjoitus17/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+
+namespace Harjoitus17
+{
+    public partial class Muistio : Form
+    {
+        string tiedostopolku = "";
+        public Muistio()
+        {
+            InitializeComponent();
+        }
+
+        private void avaaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog atk = new OpenFileDialog()
+            { Filter = "Rikastekstiformaatti|*.rtf", ValidateNames = true, Multiselect = false })
+            {
+                if (atk.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamReader vl = new StreamReader(atk.FileName))
+                    {
+                        tiedostopolku = atk.FileName;
+                        Task<string> teksti = vl.ReadToEndAsync();
+                        TekstilaatikkoRTB.Rtf = teksti.Result;
+                    }
+                }
+            }
+        }
+
+        private void uusiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TekstilaatikkoRTB.Text = "";
+        }
+
+        private void tallennaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tallennaNimelläToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tulostuksenEsikatseluToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tulostaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void poistuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
