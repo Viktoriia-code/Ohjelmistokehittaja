fa02449c97952910b6f6bef6b323b8ac4a82fe3b
Sisilisko <beloborodova.viktoriia@gmail.com>
2023-03-01 16:03:17 +0200

    init: CRUD project

---
+++ CRUD/CRUD/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.idTB = new System.Windows.Forms.TextBox();
+            this.enimiTB = new System.Windows.Forms.TextBox();
+            this.snimiTB = new System.Windows.Forms.TextBox();
+            this.puhTB = new System.Windows.Forms.TextBox();
+            this.emailTB = new System.Windows.Forms.TextBox();
+            this.oNroTB = new System.Windows.Forms.TextBox();
+            this.tyhjennaBT = new System.Windows.Forms.Button();
+            this.tallennaBT = new System.Windows.Forms.Button();
+            this.paivitaBT = new System.Windows.Forms.Button();
+            this.poistaBT = new System.Windows.Forms.Button();
+            this.TietotauluDG = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.TietotauluDG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ETUNIMI:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SUKUNIMI:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(337, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PUHELIN:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(337, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "SÄHKÖPOSTI:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(337, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(177, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "OPISKELIJANRO:";
+            // 
+            // idTB
+            // 
+            this.idTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.idTB.Location = new System.Drawing.Point(135, 20);
+            this.idTB.Name = "idTB";
+            this.idTB.Size = new System.Drawing.Size(184, 29);
+            this.idTB.TabIndex = 6;
+            // 
+            // enimiTB
+            // 
+            this.enimiTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enimiTB.Location = new System.Drawing.Point(135, 56);
+            this.enimiTB.Name = "enimiTB";
+            this.enimiTB.Size = new System.Drawing.Size(184, 29);
+            this.enimiTB.TabIndex = 7;
+            // 
+            // snimiTB
+            // 
+            this.snimiTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.snimiTB.Location = new System.Drawing.Point(135, 91);
+            this.snimiTB.Name = "snimiTB";
+            this.snimiTB.Size = new System.Drawing.Size(184, 29);
+            this.snimiTB.TabIndex = 8;
+            // 
+            // puhTB
+            // 
+            this.puhTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.puhTB.Location = new System.Drawing.Point(520, 20);
+            this.puhTB.Name = "puhTB";
+            this.puhTB.Size = new System.Drawing.Size(189, 29);
+            this.puhTB.TabIndex = 9;
+            // 
+            // emailTB
+            // 
+            this.emailTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.emailTB.Location = new System.Drawing.Point(520, 56);
+            this.emailTB.Name = "emailTB";
+            this.emailTB.Size = new System.Drawing.Size(189, 29);
+            this.emailTB.TabIndex = 10;
+            // 
+            // oNroTB
+            // 
+            this.oNroTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.oNroTB.Location = new System.Drawing.Point(520, 91);
+            this.oNroTB.Name = "oNroTB";
+            this.oNroTB.Size = new System.Drawing.Size(189, 29);
+            this.oNroTB.TabIndex = 11;
+            // 
+            // tyhjennaBT
+            // 
+            this.tyhjennaBT.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tyhjennaBT.Location = new System.Drawing.Point(13, 143);
+            this.tyhjennaBT.Name = "tyhjennaBT";
+            this.tyhjennaBT.Size = new System.Drawing.Size(145, 42);
+            this.tyhjennaBT.TabIndex = 12;
+            this.tyhjennaBT.Text = "TYHJENNÄ";
+            this.tyhjennaBT.UseVisualStyleBackColor = true;
+            this.tyhjennaBT.Click += new System.EventHandler(this.tyhjennaBT_Click);
+            // 
+            // tallennaBT
+            // 
+            this.tallennaBT.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tallennaBT.Location = new System.Drawing.Point(174, 143);
+            this.tallennaBT.Name = "tallennaBT";
+            this.tallennaBT.Size = new System.Drawing.Size(145, 42);
+            this.tallennaBT.TabIndex = 13;
+            this.tallennaBT.Text = "TALLENNA";
+            this.tallennaBT.UseVisualStyleBackColor = true;
+            this.tallennaBT.Click += new System.EventHandler(this.tallennaBT_Click);
+            // 
+            // paivitaBT
+            // 
+            this.paivitaBT.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.paivitaBT.Location = new System.Drawing.Point(337, 143);
+            this.paivitaBT.Name = "paivitaBT";
+            this.paivitaBT.Size = new System.Drawing.Size(145, 42);
+            this.paivitaBT.TabIndex = 14;
+            this.paivitaBT.Text = "PÄIVITÄ";
+            this.paivitaBT.UseVisualStyleBackColor = true;
+            this.paivitaBT.Click += new System.EventHandler(this.paivitaBT_Click);
+            // 
+            // poistaBT
+            // 
+            this.poistaBT.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.poistaBT.Location = new System.Drawing.Point(498, 143);
+            this.poistaBT.Name = "poistaBT";
+            this.poistaBT.Size = new System.Drawing.Size(145, 42);
+            this.poistaBT.TabIndex = 15;
+            this.poistaBT.Text = "POISTA";
+            this.poistaBT.UseVisualStyleBackColor = true;
+            this.poistaBT.Click += new System.EventHandler(this.poistaBT_Click);
+            // 
+            // TietotauluDG
+            // 
+            this.TietotauluDG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TietotauluDG.Location = new System.Drawing.Point(13, 194);
+            this.TietotauluDG.Name = "TietotauluDG";
+            this.TietotauluDG.Size = new System.Drawing.Size(696, 247);
+            this.TietotauluDG.TabIndex = 16;
+            this.TietotauluDG.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TietotauluDG_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 450);
+            this.Controls.Add(this.TietotauluDG);
+            this.Controls.Add(this.poistaBT);
+            this.Controls.Add(this.paivitaBT);
+            this.Controls.Add(this.tallennaBT);
+            this.Controls.Add(this.tyhjennaBT);
+            this.Controls.Add(this.oNroTB);
+            this.Controls.Add(this.emailTB);
+            this.Controls.Add(this.puhTB);
+            this.Controls.Add(this.snimiTB);
+            this.Controls.Add(this.enimiTB);
+            this.Controls.Add(this.idTB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Opiskelijarekisterin ylläpito";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TietotauluDG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox idTB;
+        private System.Windows.Forms.TextBox enimiTB;
+        private System.Windows.Forms.TextBox snimiTB;
+        private System.Windows.Forms.TextBox puhTB;
+        private System.Windows.Forms.TextBox emailTB;
+        private System.Windows.Forms.TextBox oNroTB;
+        private System.Windows.Forms.Button tyhjennaBT;
+        private System.Windows.Forms.Button tallennaBT;
+        private System.Windows.Forms.Button paivitaBT;
+        private System.Windows.Forms.Button poistaBT;
+        private System.Windows.Forms.DataGridView TietotauluDG;
+    }
+}
+
